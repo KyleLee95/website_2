5aa4ff14133d4b82c4e696594372b5be8e66f188
Kyle Lee <kyle@kylelee.dev>
2022-07-28 16:20:04 -0500

    updated readme

--- src/Home.js
+++ src/Home.js
@@ -10,15 +10,6 @@ const RelativeCamera = React.memo(function RelativeCamera(props) {
 function Box(props) {
   const { x, y, z, color, size } = props.box
 
-  // This reference gives us direct access to the THREE.Mesh object
-
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  // useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-  // Return the view, these are regular Threejs elements expressed in JSX
-
   return (
     <mesh
       //x,z,y
